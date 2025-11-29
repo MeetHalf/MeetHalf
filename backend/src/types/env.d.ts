de8c27ab737492b2fa68f89d2a8e3906e4ea7d49
Bowen Chen <bowen.chen@linecorp.com>
2025-11-29 10:33:24 +0800

    chore: change database url

--- backend/src/types/env.d.ts
+++ backend/src/types/env.d.ts
@@ -2,13 +2,13 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       PORT?: string;
-      DATABASE_URL: string;
+      DATABASE_URL?: string;
       JWT_SECRET: string;
       FRONTEND_ORIGIN?: string;
       NODE_ENV?: 'development' | 'production' | 'test';
       // Turso (libSQL) specific
-      TURSO_DATABASE_URL?: string;
-      TURSO_AUTH_TOKEN?: string;
+      DATABASE_TURSO_DATABASE_URL?: string;
+      DATABASE_TURSO_AUTH_TOKEN?: string;
     }
   }
 }
